d1dce7767578d525848cc50be9de0e1f274de4b8
Aerospace91 <beenters@gmail.com>
2025-08-12 10:20:56 -0700

    Ugh Linked Lists

---
+++ linked_list.py
@@ -0,0 +1,45 @@
+from typing import Optional, Any
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+    
+    def set_next(self, node):
+        self.next = node
+    
+    def __repr__(self):
+        return self.val
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def add_to_head(self, node):
+        if self.head is None:
+            self.tail = node
+        node.set_next(self.head)
+        self.head = node
+
+    def add_to_tail(self, node):
+        if self.head is None:
+            self.head = node
+            self.tail = node
+            return
+        self.tail.set_next(node)
+        self.tail = node
+        
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+            
+    def __repr__(self):
+        nodes = []
+        current = self.head
+        while current and hasattr(current, "val"):
+            nodes.append(current.val)
+            current = current.next
+        return " -> ".join(nodes)
